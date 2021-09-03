fbb0dc15a41f1f7ae6d8ce3e7c547819adb4b3fb
NETBOSS <paguti032020@hotmail.com>
2021-09-02 21:00:27 -0600

    Decidi cambiarme mejor a jupyter, he agregado 2 columnas, meses y ventas para respoder las 2 preguntas

--- SalesAnalysis.py
+++ SalesAnalysis.py
@@ -11,8 +11,23 @@ files = [file for file in os.listdir('./Sales_Data')]
 
 all_months_data = pd.DataFrame()
 # por cada csv se ira agregando al dataframe all_months_data para tener un archivo con todos los datos
+'''
 for file in files:
     df = pd.read_csv("./Sales_Data/"+file)
     all_months_data = pd.concat([all_months_data, df])
 print(all_months_data.head())
-#print(df.head())
+
+all_months_data.to_csv('all_data.csv') #guardandolo como archivo csv
+'''
+
+all_data = pd.read_csv("all_data.csv")
+#print(all_data.head())
+
+#Task#2: Agregando columna de meses
+
+#Q1: What was the best month for sales? How much was earned that month?
+
+all_data['Month'] = all_data['Order Date'].str[0:2]
+all_data['Month'] = all_data['Month'].astype('int32')
+print(all_data.head())
+
